3b78218cf715167477aef3c7696cfdcb91c2e232
thejuniordevops <edigithub@gmail.com>
2019-01-01 14:23:49 +0700

    add css

--- src/components/CSStransition.js
+++ src/components/CSStransition.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
+import {CSSTransition} from 'react-transition-group';
 
 import '../css/App.css';
 
 class Fade extends Component{
+
+    state = {
+        show:false
+    }
+
+    showDiv = () => {
+        this.setState({
+            show:!this.state.show ? true : false
+        });
+        console.log(this.state);
+    };
+    
+
     render(){
         return(
             <div>
-                CSS TRANSITIONS
+                <CSSTransition
+                    in={this.state.show}
+                    timeout={5000}
+                    classNames="square"
+
+                    enter={true}
+                >
+
+                    <div className={`square ${this.state.show}`}>
+                        Hello
+                    </div>
+
+                </CSSTransition>
+
+                <div className="showDiv" 
+                 onClick={this.showDiv}>
+                    SHow or hide
+                </div>
+                
             </div>
         )
     }
--- src/components/Transition.js
+++ src/components/Transition.js
@@ -1,12 +1,60 @@
 import React, { Component } from 'react';
 import '../css/App.css';
+import ReactTransition from 'react-transition-group/Transition';
 
 class TransitionComp extends Component{
+
+    state = {
+        show:false
+    }
+
+    showDiv = () => {
+        this.setState({
+            show:!this.state.show ? true : false
+        })
+    };
 
     render(){
         return(
             <div>
-                Transition
+                <ReactTransition
+                    in={this.state.show}
+                    timeout={{
+                        enter:2000,
+                        exit:50
+                    }}
+
+                    enter={true}
+                    exit={true}
+
+                    onEnter={ (node) => {
+                        console.log('Enter')
+                    }}
+
+                    onExit={ (node) => {
+                        console.log('Exited')
+                    }}
+
+                   
+
+                   
+                >
+                    {state => 
+                    
+                        <div className={`square square-${state}`}>
+                            { `square square-${state}` }
+                        </div>
+                
+                }
+
+                </ReactTransition>
+
+                <div className="showDiv"
+                    onClick = {this.showDiv}
+                >
+                    show or hide
+                </div>
+                
             </div>
 
         )
